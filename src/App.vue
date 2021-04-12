eeebe622928b4d0c4faea2913edcf4d93046dbb5
tianhaoAA <1334750953@qq.com>
2021-04-12 22:18:11 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 	export default {
 		// 执行一次
 		onLaunch: function() {
-			console.log('App Launch 项目启动了')
+			// console.log('App Launch 项目启动了')
 		},
 		onShow: function() {
-			console.log('App Show 项目显示了')
+			// console.log('App Show 项目显示了')
 		},
 		onHide: function() {
-			console.log('App Hide 项目隐藏了')
+			// console.log('App Hide 项目隐藏了')
 		}
 	}
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,50 +1,22 @@
 <template>
-	<view class="content">
-	
-	{{name}}
-		<view >
-          <text v-for="(item,index) in list " :key="index">我是谁啊 {{item.name}}</text>  
-		</view>
-		<button @click="chang('123',$event)">点击修改msg</button>
-	</view>
+  <view>首页 </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello',
-				name:'张三',
-				list:[{name:'张三'},{name:'李四'},]
-			}
-		},
-		onLoad() {
-			// 执行一次
-          console.log('onLoad 首页刚刚加载 ');
-		},
-		onShow(){
-		 console.log('onShow 页面显示了');
-		},
-		onReay(){
-			// 执行一次
-			 console.log('onShow 页面初次渲染了');
-		},
-		onHide(){
-			console.log('onHide 页面隐藏了')
-		},
-		onUnload(){
-			console.log('onUnload 页面卸载了')
-		},
-		methods: {
-            chang(id,event){
-				this.name='李四'
-				console.log(id);
-				console.log('事件对象E',event);
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+    
+    };
+  },
+  onLoad() {
+  
+ 
+  },
+
+  methods: {},
+};
 </script>
 
 <style>
-
 </style>
